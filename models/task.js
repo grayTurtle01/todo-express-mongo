82ae2face710320e1694a29f6d6fc556170823b7
grayTurtle01 <the.correo@protonmail.com>
2022-06-08 14:09:44 -0500

    mongoose implementation

---
+++ models/task.js
@@ -0,0 +1,19 @@
+/*** Mongoose ***/
+const mongoose = require('mongoose')
+
+const uri = process.env.MONGO_URI || "mongodb://localhost/miDB"
+
+mongoose.connect(uri)
+.then( db => console.log(' ==> db connected by mongoose'))
+.catch(err => console.log(err))
+
+
+/*** Model  ****/
+var taskSchema = mongoose.Schema({
+    content: String,
+    done: Boolean 
+})
+
+const Task = mongoose.model('tasks', taskSchema)
+
+module.exports = Task
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 express = require('express')
 bodyParser = require('body-parser')
-require('./db')
 
-mongo = require('mongodb')
 
 app = express()
 
@@ -14,19 +12,24 @@ app.use( bodyParser.urlencoded({extended:true}))
 app.use(express.json())
 app.use(express.static('public'))
 
+
+/*** Models ****/
+const Task = require('./models/task')
+
 /*** Routes ***/
 app.get("/", (req,res) => {
     
 
-    db.collection('tasks').find().toArray()
-    .then( data => {
-        res.render('index.ejs', {tasks: data})
-    })
+    Task.find({})
+        .then( array => {
+            res.render('index.ejs', {tasks: array})
+        })
+        .catch( err => res.json({error: err}))
     
 })
 
  app.get("/tasks", (req,res) => {
-     db.collection('tasks').find().toArray()
+     Task.find({})
      .then( data => {
          res.json(data)
      })
@@ -39,60 +42,53 @@ app.post("/tasks", (req, res) => {
         done: false
     }
 
-    db.collection('tasks').insertOne( new_task )
+    Task.create( new_task )
         .then( x => {
-            // console.log(x)
             res.redirect('/')
-     })
+        })
     
 })
 
 app.delete("/tasks", (req, res) =>{
     id = req.body.id
 
-    db.collection('tasks').deleteOne( {'_id': mongo.ObjectId(id)})
-    .then( x => {
-       if( x.deletedCount == 0){
-            res.json({status: 'task not found'})
-        }
-       else{
-            res.json({status: 'task deleted'})
-        }
-            
-    })
+    Task.deleteOne( {'_id': id })
+        .then( x => {
+        if( x.deletedCount == 0){
+                res.json({status: 'task not found'})
+            }
+        else{
+                res.json({status: 'task deleted'})
+            }
+                
+        })
     
 })
 
 app.get("/tasks/edit/:id", (req,res) => {
 
-    id = mongo.ObjectId( req.params.id )
-    db.collection('tasks').findOne({'_id': id})
+    
+    Task.findById( req.params.id )
         .then( task => {
-            //~ console.log(task)
             res.render('edit.ejs', {'task': task})
         })
     
 })
 
 
-
 app.put("/tasks", (req, res)=> {
     id = req.body.id
     content = req.body.content
 
-    query = { '_id': mongo.ObjectId(id) }
-    new_values = { $set: {'content': content } }
-    options = {'upsert': true}
+  
+    Task.findById(id)
+        .then( task => {
+            task.content = content
+            task.save()
 
-    db.collection('tasks').findOneAndUpdate(
-                             query,
-                             new_values,
-                             options)
-        .then(x => {
-            //~ console.log(x)
-            res.json({status: 'Task Updated'})
+            res.json(task)
         })
-        .catch( err => console.log(err)) 
+                           
 })
 
 
@@ -100,29 +96,14 @@ app.put("/tasks", (req, res)=> {
 app.get("/tasks/toggle/:id", (req, res) =>{
     id = req.params.id
     
-    // Modelo.findById(id, (err, task) => {
-    //     if(err)
-    //         res.json({status: err})
+    Task.findById(id, (err, task) => {
+        if(err)
+            res.status(404).json({status: err})
 
-    //     task.done = !task.done
-    //     task.save()
-    //     res.json( task )
-    // })
-
-    query = {'_id': mongo.ObjectId(id)}
-    new_values = [
-        {$set: { done: {$not: "$done"} } }
-        // {$set: { done: {$eq:[false, "$done"]} } }
-        
-    ]
-
-    db.collection('tasks').findOneAndUpdate( query, new_values)
-    .then( x => {
-        task = x.value
+        task.done = !task.done
+        task.save()
         res.json( task )
     })
-    .catch( err => console.log(err))
-
     
 })
 
