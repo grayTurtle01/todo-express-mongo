94a62e070816cf7abdcc51cfbf1a1df30cc944be
grayTurtle01 <the.correo@protonmail.com>
2022-06-22 07:32:42 -0500

    MVC Structure

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -65,11 +65,112 @@ let resetLikes = async (req,res) => {
     res.redirect('/')
 }
 
+let renderEditor = (req,res) => {
+   
+    Task.findById( req.params.id )
+        .then( task => {
+            res.render('edit.ejs', {'task': task})
+        })
+    
+}
+
+let updateTask = (req, res)=> {
+    const id = req.body.id
+    const content = req.body.content
+
+  
+    Task.findById(id)
+        .then( task => {
+            task.content = content
+            task.save()
+
+            res.json(task)
+        })
+                           
+}
+
+let flipState = (req, res) =>{
+    let id = req.params.id
+    
+    Task.findById(id, (err, task) => {
+        if(err)
+            res.status(404).json({status: err})
+
+        task.done = !task.done
+        task.save()
+        res.json( task )
+    })
+    
+}
+
+let addLike = (req, res) => {
+
+    Task.findById(req.body.id)
+        .then( task => {
+            task.likes = task.likes + 1
+            task.save()
+            res.json(task)
+        })
+        .catch( err => res.json({error: err}))
+}
+
+let uppRow = (req, res) => {
+    
+    Task.findById(req.params.id)
+        .then( task => {
+            if( task.position == 0){
+                res.json({status: 'the row is on the top'})
+            }
+            else{
+                Task.findOne({position : task.position - 1 })
+                    .then( upperTask => {
+                        let tmp = task.position
+                        task.position = upperTask.position
+                        upperTask.position = tmp
+
+                        task.save()
+                        upperTask.save()    
+
+                        res.json({status: 'rows exchanged'})    
+                    } )
+                    .catch( err => console.log(err))
+            }
+        })
+
+}
+
+let downRow = async (req, res) => {
+
+    let task  = await Task.findById(req.params.id)
+
+    if( task.position == (rows-1)  ){
+        res.json({status: 'the last row'})
+    }
+    else{
+        let lowerTask = await Task.findOne({position: task.position + 1})
+
+        let tmp = task.position
+        task.position = lowerTask.position
+        lowerTask.position = tmp
+
+        task.save()
+        lowerTask.save()
+
+        res.json({status: 'rows exchanged'})
+    }
+}
+
 module.exports = {
     renderTasks,
     getAllTasks,
     getTask,
     addTask,
     deleteTask,
-    resetLikes
+    resetLikes,
+    renderEditor,
+    updateTask,
+    flipState,
+    addLike,
+    uppRow,
+    downRow
 }
--- routes/tasks.js
+++ routes/tasks.js
@@ -20,106 +20,22 @@ router.delete('/tasks', controller.deleteTask)
 
 router.get('/reset', controller.resetLikes)
 
-router.get('/tasks/edit/:id', (req,res) => {
-
-    
-    Task.findById( req.params.id )
-        .then( task => {
-            res.render('edit.ejs', {'task': task})
-        })
-    
-})
+router.get('/tasks/edit/:id', controller.renderEditor)
 
 
-router.put('/tasks', (req, res)=> {
-    const id = req.body.id
-    const content = req.body.content
-
-  
-    Task.findById(id)
-        .then( task => {
-            task.content = content
-            task.save()
-
-            res.json(task)
-        })
-                           
-})
+router.put('/tasks', controller.updateTask)
 
 
 // Flip State
-router.get('/tasks/toggle/:id', (req, res) =>{
-    let id = req.params.id
-    
-    Task.findById(id, (err, task) => {
-        if(err)
-            res.status(404).json({status: err})
-
-        task.done = !task.done
-        task.save()
-        res.json( task )
-    })
-    
-})
+router.get('/tasks/toggle/:id', controller.flipState)
 
 // Add Like
-router.put('/tasks/addLike', (req, res) => {
-
-    Task.findById(req.body.id)
-        .then( task => {
-            task.likes = task.likes + 1
-            task.save()
-            res.json(task)
-        })
-        .catch( err => res.json({error: err}))
-})
+router.put('/tasks/addLike', controller.addLike)
 
 // UpRow
-router.get('/tasks/upRow/:id', (req, res) => {
-    
-    Task.findById(req.params.id)
-        .then( task => {
-            if( task.position == 0){
-                res.json({status: 'the row is on the top'})
-            }
-            else{
-                Task.findOne({position : task.position - 1 })
-                    .then( upperTask => {
-                        let tmp = task.position
-                        task.position = upperTask.position
-                        upperTask.position = tmp
+router.get('/tasks/upRow/:id', controller.uppRow)
 
-                        task.save()
-                        upperTask.save()    
-
-                        res.json({status: 'rows exchanged'})    
-                    } )
-                    .catch( err => console.log(err))
-            }
-        })
-
-})
-
-router.get('/tasks/downRow/:id', async (req, res) => {
-
-    let task  = await Task.findById(req.params.id)
-
-    if( task.position == (rows-1)  ){
-        res.json({status: 'the last row'})
-    }
-    else{
-        let lowerTask = await Task.findOne({position: task.position + 1})
-
-        let tmp = task.position
-        task.position = lowerTask.position
-        lowerTask.position = tmp
-
-        task.save()
-        lowerTask.save()
-
-        res.json({status: 'rows exchanged'})
-    }
-})
+router.get('/tasks/downRow/:id', controller.downRow)
 
 
 
